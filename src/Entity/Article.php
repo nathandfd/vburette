0456595d8f1fb73f44a8ea30e7bb7a8d04ad3b94
NATHAN DUFAUD <nathan.dufaud@etudiant.univ-reims.fr>
2021-04-12 11:31:54 +0200

    update

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
