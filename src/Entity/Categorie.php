745277975cd320d3d0c18a72129bd5a9372033cc
NATHAN DUFAUD <nathan.dufaud@etudiant.univ-reims.fr>
2021-04-12 11:36:32 +0200

    update

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -7,6 +7,9 @@ use App\Repository\CategorieRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 /**
  * @ApiResource()
