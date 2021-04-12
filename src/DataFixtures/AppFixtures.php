a5c857e013bdc37efe6b53b74c475285a0c72687
NATHAN DUFAUD <nathan.dufaud@etudiant.univ-reims.fr>
2021-04-12 11:01:02 +0200

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
@@ -12,15 +14,30 @@ class AppFixtures extends Fixture
     {
         $faker = \Faker\Factory::create();
 
-        for ($i = 0; $i < 1; $i++) {
+        for ($i = 0; $i < 150; $i++) {
             $article = new Article();
             $article->setName($faker->name);
             $article->setDescription($faker->text);
             $article->setUrlImage($faker->imageUrl($width = 640, $height = 480));
-            $article->setCategorie($faker->randomElement($array = array ('Maison','Bien-être','Cuisine','Salle de bain','Garage','Cosmétique','Homme','Femme')));
+            $article->setCategorie($faker->randomElement($array = array ('Maison','Bien-être','Cuisine','Salle de bain','Garage','Cosmétique','Homme','Femme','Enfant','Jouet')));
             $article->setPrice(mt_rand(10, 100));
             $article->setCreatedAt($faker->datetime);
             $manager->persist($article);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $categorie = new Categorie();
+            $categorie->setName($faker->randomElement($array = array ('Maison','Bien-être','Cuisine','Salle de bain','Garage','Cosmétique','Homme','Femme','Enfant','Jouet'), $count = $i));
+            $categorie->setCreatedAt($faker->datetime);
+            $manager->persist($categorie);
+        }
+
+        for ($i = 0; $i < 50; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setFullname($faker->firstName);
+            $user->setCreatedAt($faker->datetime);
+            $manager->persist($user);
         }
 
         $manager->flush();
