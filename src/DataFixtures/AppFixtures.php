d8a5006be4472645a63b3bdcd3973ffcd0efbb18
NATHAN DUFAUD <nathan.dufaud@etudiant.univ-reims.fr>
2021-04-12 10:37:22 +0200

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,8 +9,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 1; $i++) {
+            $article = new Article();
+            $article->setName($faker->name);
+            $article->setDescription($faker->name)
+            $article->setUrlImage($faker->name)
+            $article->setCategorie($faker->name)
+            $article->setPrice(mt_rand(10, 100));
+            $article->setCreatedAt($faker->datetime)
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
