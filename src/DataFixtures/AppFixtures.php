28fa2f2cf580c143f8f79455e72cf4f963b11a5a
NATHAN DUFAUD <nathan.dufaud@etudiant.univ-reims.fr>
2021-04-12 10:54:21 +0200

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,9 +15,9 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < 1; $i++) {
             $article = new Article();
             $article->setName($faker->name);
-            $article->setDescription($faker->name);
-            $article->setUrlImage($faker->name);
-            $article->setCategorie($faker->name);
+            $article->setDescription($faker->text);
+            $article->setUrlImage($faker->imageUrl($width = 640, $height = 480));
+            $article->setCategorie($faker->randomElement($array = array ('Maison','Bien-être','Cuisine','Salle de bain','Garage','Cosmétique','Homme','Femme')));
             $article->setPrice(mt_rand(10, 100));
             $article->setCreatedAt($faker->datetime);
             $manager->persist($article);
